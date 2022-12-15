6cc0455f54d1d62a52552a7b1bde4d1919eb33f8
Bairoking <gs8004rtj5jj5j@gmail.com>
2022-12-15 14:28:32 -0800

    Advanced Solution

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -1,8 +1,14 @@
 #include "binary_trees.h"
+#include "limits.h"
+
+size_t height(const binary_tree_t *tree);
+int is_avl_helper(const binary_tree_t *tree, int lo, int hi);
+int binary_tree_is_avl(const binary_tree_t *tree);
 
 /**
  * height - Measures the height of a binary tree.
  * @tree: A pointer to the root node of the tree to measure the height.
+ *
  * Return: If tree is NULL, your function must return 0, else return height.
  */
 size_t height(const binary_tree_t *tree)
@@ -21,33 +27,34 @@ size_t height(const binary_tree_t *tree)
 /**
  * is_avl_helper - Checks if a binary tree is a valid AVL tree.
  * @tree: A pointer to the root node of the tree to check.
- * @sm: The value of the smallest node visited thus far.
- * @la: The value of the largest node visited this far.
+ * @lo: The value of the smallest node visited thus far.
+ * @hi: The value of the largest node visited this far.
  *
  * Return: If the tree is a valid AVL tree, 1, otherwise, 0.
  */
-int is_avl_helper(const binary_tree_t *tree, int sm, int la)
+int is_avl_helper(const binary_tree_t *tree, int lo, int hi)
 {
 	size_t lhgt, rhgt, diff;
 
 	if (tree != NULL)
 	{
-		if (tree->n < sm || tree->n > la)
+		if (tree->n < lo || tree->n > hi)
 			return (0);
 		lhgt = height(tree->left);
 		rhgt = height(tree->right);
 		diff = lhgt > rhgt ? lhgt - rhgt : rhgt - lhgt;
 		if (diff > 1)
 			return (0);
-		return (is_avl_helper(tree->left, sm, tree->n - 1) &&
-			is_avl_helper(tree->right, tree->n + 1, la));
+		return (is_avl_helper(tree->left, lo, tree->n - 1) &&
+			is_avl_helper(tree->right, tree->n + 1, hi));
 	}
 	return (1);
 }
 
 /**
  * binary_tree_is_avl - Checks if a binary tree is a valid AVL tree.
- * @tree: pointer to the root node of the tree to check.
+ * @tree: A pointer to the root node of the tree to check.
+ *
  * Return: 1 if tree is a valid AVL tree, and 0 otherwise
  */
 int binary_tree_is_avl(const binary_tree_t *tree)
